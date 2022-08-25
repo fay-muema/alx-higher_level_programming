2fb38fd2243be09a37d738f56e38e2c68756605b
fay-muema <faithmuema45@gmail.com>
2022-08-25 11:46:16 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print(len(sys.argv))
+
+    arg = len(sys.argv) - 1
+    
+    if arg == 0:
+        print("{} argument:".format(arg))
+
+    elif arg == 1:
+        print("{} argument:".format(arg))
+    else:
+        print("{} argument:".format(arg))
+
+    if arg >= 1:
+        arg = 0
+
+        for i in sys.argv:
+            if arg != 0:
+                print("{}: {}".format(arg, i))
+
+            arg += 1
