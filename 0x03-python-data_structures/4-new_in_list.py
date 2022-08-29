fde7cbe69208637917a288363c9ff650f2bf6e65
fay-muema <faithmuema45@gmail.com>
2022-08-29 12:01:58 -0700

    lists

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+    
+    length = len(my_string)
+
+    if idx > length - 1:
+        return (my_list)
+
+    my_list[idx] = element
+
+    return (my_list)
