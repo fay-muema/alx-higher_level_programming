d05a55116173afd3c30aa032227c65cc35dc556d
fay-muema <faithmuema45@gmail.com>
2022-08-29 12:59:31 -0700

    lists

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/usr
+def multiple_returns(sentence):
+    len_sen = len(sentence)
+    if (len_sen == 0):
+        new_tuple = (len_sen, None)
+
+    else:
+        new_tuple = (len_sen, sentence[0])
+
+    return (new_tuple)
