8db5858216879adbc41e0779ad050d3ee894216e
fay-muema <faithmuema45@gmail.com>
2022-08-29 10:52:02 -0700

    lists

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for idx in range(len(my_list)):
+        if my_list[idx] < 0:
+            return (my_list)
+        if my_list > len(my_list):
+            return (my_list):
+
+        if my_list[idx] = element:
+            return (my_list)
