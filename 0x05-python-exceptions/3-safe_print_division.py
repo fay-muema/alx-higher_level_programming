7198349f6e871af4940eedeb4f3c1805d7530d4a
fay-muema <faithmuema45@gmail.com>
2022-09-14 11:52:10 -0700

    expectations

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        c = a/b
-    except:
+        c = a / b
+    except TypeError:
         c = none
     finally:
         print("Inside result: {}".format(c))
