a369509ff82907fcda8c2185b9dedd329b978093
fay-muema <faithmuema45@gmail.com>
2022-09-15 13:21:24 -0700

    classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,12 +2,4 @@
 class Square:
     """Class Square that defines a square.
     """
-    pass#!/usr/bin/python3
-class Square:
-    """Class Square that defines a square.
-    """
-    pass#!/usr/bin/python3
-class Square:
-    """Class Square that defines a square.
-    """
     pass
