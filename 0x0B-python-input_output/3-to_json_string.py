32630ad0b6f163f38c755b96e3cb3514eabcc498
fay-muema <faithmuema45@gmail.com>
2022-09-27 11:06:43 -0700

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    to_json_string - returns json representation
+"""
+def to_json_string(my_obj):
+    """
+    function that returns the JSON representation of an object
+    """
+    return json.dumps(my_obj)
