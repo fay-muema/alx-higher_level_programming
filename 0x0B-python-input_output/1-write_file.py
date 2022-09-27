955aaac41aec54d9c26866104278105dbedf6739
fay-muema <faithmuema45@gmail.com>
2022-09-27 09:13:44 -0700

    files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""
-    write_file - writes line to a file
+""" write_file - writes line to a file
 """
 
 
 def write_file(filename="", text=""):
     """
-   function that writes string to the file
-   """
-   with open(filename, 'w', encoding="UTF8") as f:
-       return f.write(text)
+    function that writes string to the file
+    """
+    with open(filename, 'w', encoding="UTF8") as f:
+        return f.write(text)
