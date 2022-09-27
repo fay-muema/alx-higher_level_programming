add894eced126fbddbd9656eb426c63ad27878fa
fay-muema <faithmuema45@gmail.com>
2022-09-27 12:14:45 -0700

    json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,10 @@ import os.path
 save_file = __import__('5-save_to_json_file').save_to_json_file
 load_file = __import__('6-load_from_json_file').load_from_json_file
 
+"""
+script that adds all arguments to a Python list,
+and then save them to a file
+"""
 my_list = []
 if os.path.exists("add_item.json"):
     my_list = load_file("add_item.json")
