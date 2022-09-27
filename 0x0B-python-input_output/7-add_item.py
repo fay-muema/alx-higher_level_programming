56de0bd02dbb01113105f3a746306b17cbc75645
fay-muema <faithmuema45@gmail.com>
2022-09-27 12:12:32 -0700

    json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ import os.path
 save_file = __import__('5-save_to_json_file').save_to_json_file
 load_file = __import__('6-load_from_json_file').load_from_json_file
 
-my_lsit = []
+my_list = []
 if os.path.exists("add_item.json"):
     my_list = load_file("add_item.json")
 
