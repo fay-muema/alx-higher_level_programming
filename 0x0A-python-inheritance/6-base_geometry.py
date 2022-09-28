6a4def9a18d4e76b7eb6d4947da933bc77a3712f
fay-muema <faithmuema45@gmail.com>
2022-09-28 00:43:14 -0700

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Emmpty class
+    """
+
+    def area(self):
+        """
+        public instance methpd
+        """
+        raise Exception("area() is not implemented")
