faaa1e4cbe23b28eafc3445cc83dce33f6f9b432
fay-muema <faithmuema45@gmail.com>
2023-01-02 06:07:51 -0800

    loops

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const size = parseInt(process.argument[2]);
+if (Number.isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0, s; i < size; i++) {
+    s = '';
+    for (let j = 0; j < size; j++) {
+      s += 'X';
+    }
+    console.log(s);
+  }
+}
