1b47316c8d8ee8ff9a7f02e86dabb0d591861600
fay-muema <faithmuema45@gmail.com>
2023-01-02 01:24:44 -0800

    javascript to print 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-const lines = ' C is fun /n Python is cool /n JavaScript is amazing ';
-console.log(lines);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
