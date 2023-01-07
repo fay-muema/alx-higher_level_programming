6c364f9a51ccd7add26afbd84e4a99598e32b31f
fay-muema <faithmuema45@gmail.com>
2023-01-07 04:36:44 -0800

    biggest number among arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -10,6 +10,7 @@ for (i = 2; i < process.argv.length; i++) {
 
 if (arrayNumbers.length > 1) {
   biggest = Math.max.apply(null, arrayNumbers);
+  i = arrayNumbers.indexOf(biggest);
   arrayNumbers[i] = -Infinity;
   biggest = Math.max.apply(null, arrayNumbers);
 }
