e4c2d78d734a52cb43e7efb8717772b4d7e014d7
fay-muema <faithmuema45@gmail.com>
2023-01-09 05:39:49 -0800

    increasing value

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,8 +1,20 @@
 #!/usr/bin/node
 const myObject = {
-	type: 'object',
-	value: 12
-},
-const incr = {
-	myObject.value = ++value;
+  type: 'object',
+  value: 12
 };
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+
+console.log(myObject);
+
+myObject.incr();
+
+console.log(myObject);
+
+myObject.incr();
+
+console.log(myObject);
